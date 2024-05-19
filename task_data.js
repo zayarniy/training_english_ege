d60a0217cced2e88a519cf04d68359e05a768c64
Your Name <you@example.com>
2024-05-19 18:55:59 +0300

    added 3-5 variants

--- task_data.js
+++ task_data.js
@@ -68,10 +68,177 @@ let Task2 =
     text2: "<strong>You will speak for not more than 3 minutes (12-15 sentences). You have to talk continuously.</strong>"
   },
   filename: 'tasks/2/task2.pdf'
-
 }
 
-const AllTasks = [0, Task1, Task2]
+
+let Task3 =
+{
+  task1:
+  {
+    header: 'Task 1. Imagine that you are preparing a project with your friend. You have found some interesting material for the presentation and you want to read this text to your friend. You have 1.5 minutes to read the text silently, then be ready to read it out aloud. You will not have more than 1.5 minutes to read it.',
+    text: 'The term “human migration” forms a major part of human history. It has been practiced from historic times in various parts of the globe and for various reasons. Generally, the concept is defined as the movement or change from one geographical area to another, as well as across the border. It involves external migration, which means movement across an internationally recognized border, i.e. moving from one country to the other or one continent to another.  Internal migration means movement within a recognized border, located in the same state or country, including rural migrations. There are some core truths and joys about country living and rural migrations. Urban areas typically have a large population, a high population density and much social and economic infrastructure and amenities.  Moving to a favourable rural area to live and work there discover once they’ve made the move. However, the rates of such migrate movements differ, with generally high rates seen in developing nations.'
+  },
+  task2:
+  {
+    header1: "Task 2. You are considering visiting the lama park and you'd like to get more information. In 1.5 minutes you are to ask four direct questions to find out the following:.",
+    header2: "",
+    header3: "",
+    image: 'tasks/3/lama.jpg',
+    text1: `1) location<br>
+    2) opening hours<br>
+    3) age restrictions<br>
+    4) organized tours<br>`,
+    text2: '<br><strong>You have 20 seconds to ask each question.</strong>',
+    questions: ['membership fee', 'frequency of online meetings', 'duration of group discussions', 'if members receive discussion guides']
+  },
+  task3:
+  {
+    header: 'Task 3. You are going to give an interview. You have to answer five questions. Give full answers to the questions (2–3 sentences). Remember that you have 40 seconds to answer each question.',
+    introduction: `Hello everybody! It’s Teenagers Round the World Channel. Our 
+    guest today is a teenager from Russia and we are going to discuss teenagers’ 
+    attitude to different places of living. We’d like to know our guest’s point of view on this issue. Please answer five questions. So, let’s get started`,
+    introduction1: "Hello everybody! It’s Teenagers Round the World Channel. Our guest today is a teenager from Russia and we are going to discuss teenagers’ attitude to different places of living. ",
+    introduction2: "We’d like to know our guest’s point of view on this issue. Please answer five questions. So, let’s get started.",
+    interviewer: ["Is the city life stressful? Why?", "How would you improve the living conditions in your area?", "Why do young people tend to live in big cities?", "Where do you think people will live in the future: in the cities or in the countryside? Why?", "Would you like to change the place where you live? Why?"]
+  },
+  task4:
+  {
+    header: 'Task 4. Imagine that you and your friend are doing a school project “Urban and Suburban Living”. You have found some illustrations and want to share the news. Leave a voice message to your friend. In 2,5 minutes be ready to:',
+    text: `explain the choice of the illustrations for the project by briefly describing them and noting the differences;<br>
+    • mention the advantages (1 − 2) of the two kinds of residency;<br>
+    • mention the disadvantages (1 − 2) of the two kinds of residency;<br>
+    • express your opinion on the subject of the project — what kind of residency you would prefer and why.
+    `,
+    images: ['tasks/3/4-1.jpg', 'tasks/3/4-2.jpg'],
+    text2: "<strong>You will speak for not more than 3 minutes (12-15 sentences). You have to talk continuously.</strong>"
+  },
+  filename: 'tasks/3/task3.pdf'
+}
+
+
+
+let Task4 =
+{
+  task1:
+  {
+    header: 'Task 1. Imagine that you are preparing a project with your friend. You have found some interesting material for the presentation and you want to read this text to your friend. You have 1.5 minutes to read the text silently, then be ready to read it out aloud. You will not have more than 1.5 minutes to read it.',
+    text: `The interests of modern society have changed a lot in the last few years. A recent
+    survey shows some interesting results. In the past, people's hobbies were often things
+    like gardening or knitting, but now many people are interested in new experiences
+    and adventures. They want to travel to different countries and try new sports like
+    surfing or mountain biking.
+    Volunteering is another activity that is more common among young people. Over half
+    of those under 25 do some kind of voluntary work, while only 10% of those over 65
+    help out in their local community. Volunteering can be hard work, but it can also be
+    very rewarding and fun. There are many different types of volunteering opportunities
+    available. If you enjoy working with children, you could volunteer at a local school
+    or community center. If you're interested in the environment, you could join a
+    conservation group and help clean up parks and beaches, plant trees, or protect
+    endangered species. Whatever type of volunteering you choose, it's important to
+    remember that your time and effort can make a big difference.`
+
+  },
+  task2:
+  {
+    header1: "Task 2. Study the advertisement.",
+    header2: "Slow down and relax!",
+    header3: `You are considering joining the yoga club and now you’d like to get more
+    information. In 1.5 minutes you are to ask four direct questions to find out about
+    the following:`,
+    image: 'tasks/4/relax.jpg',
+    text1: `1) location<br>
+    2) number of people in a group<br>
+    3) duration of a class<br>
+    4) pensioner and student discounts`,
+    text2: '<br><strong>You have 20 seconds to ask each question.</strong>',
+    questions: ['location', 'number of people in a group', 'duration of a class', 'pensioner and student discounts']
+  },
+  task3:
+  {
+    header: `Task 3. You are going to give an interview. You have to answer five questions.
+    Give full answers to the questions (2–3 sentences).
+    Remember that you have 40 seconds to answer each question`,
+    introduction: `Task 3. Hello everybody! It’s Teenagers Round the World Channel. Our
+    guest today is a teenager from Russia and we are going to discuss teenagers’
+    attitude to volunteering. We’d like to know our guest’s point of view on
+    this issue. Please answer five questions. So, let’s get started`,
+    introduction1: "Hello everybody! It’s Teenagers Round the World Channel. Our guest today is a teenager from Russia and we are going to discuss teenagers’ attitude to volunteering.",
+    introduction2: "We’d like to know our guest’s point of view on this issue. Please answer five questions. So, let’s get started.",
+    interviewer: ["What kind of volunteer work do you think is important in your region?", "Have you ever volunteered for an organization?", "Do you prefer volunteering alone or with a group of friends? Why?", "What skills do you think are important for volunteers to have?", "What advice would you give to someone who is considering getting involved in volunteer work?"]
+  },
+  task4:
+  {
+    header: `Task 4. Imagine that you and your friend are doing a school project “Outstanding hobbies”. You have found
+    some photos to illustrate it but for technical reasons you cannot send them now. Leave a voice
+    message to your friend explaining your choice of the photos and sharing some ideas about the
+    project. In 2.5 minutes be ready to:`,
+    text: `• explain the choice of the illustrations for the project by briefly describing
+them and noting the differences;<br>
+• mention the advantages (1–2) of the two hobbies;<br>
+• mention the disadvantages (1–2) of the two hobbies;<br>
+• express your opinion on the subject of the project – which of the hobbies presented in the
+pictures you’d prefer and why.`,
+    images: ['tasks/4/4-1.jpg', 'tasks/4/4-2.jpg'],
+    text2: "<strong>You will speak for not more than 3 minutes (12-15 sentences). You have to talk continuously.</strong>"
+  },
+  filename: 'tasks/4/task4.pdf'
+}
+
+let Task5 =
+{
+  task1:
+  {
+    header: 'Task 1. Imagine that you are preparing a project with your friend. You have found some interesting material for the presentation and you want to read this text to your friend. You have 1.5 minutes to read the text silently, then be ready to read it out aloud. You will not have more than 1.5 minutes to read it.',
+    text: `Choosing a job is one of the most important decisions we make in our lives. We spend a lot of time at work, so it's important to choose something we enjoy and that makes us happy.
+    Some people choose a job because it pays well. Others choose a job because their friends or family work in it. Some people choose a job because it's interesting or exciting. Other people choose a job because they want to help others.
+    When I was at school, I didn't know what job I wanted to do. I liked maths and science, so I thought about being a doctor or an engineer. But then I realised I didn't like hospitals and I didn't want to be in an office all day. In the end, I started studying languages, and now I'm a translator. I love my job because I can work from home and I can travel a lot.
+    `
+
+  },
+  task2:
+  {
+    header1: "Task 2. Study the advertisement.",
+    header2: "New opportunities with Russian New Tech.",
+    header3: `You are considering going to the job interview and now you’d like to get more information.<br>
+    In 1.5 minutes you are to ask four direct questions to find out about the following:`,
+    image: 'tasks/5/2.jpg',
+    text1: `1. Location<br>
+    2. Duration<br>
+    3. Salary<br>
+    4. Bonuses`,
+    text2: '<br><strong>You have 20 seconds to ask each question.</strong>',
+    questions: ['Location', 'Duration', 'Salary', 'Bonuses']
+  },
+  task3:
+  {
+    header: `Task 3. You are going to give an interview. You have to answer five questions. Give full answers to the questions (2-3 sentences).
+    Remember that you have 40 seconds to answer each question.`,
+    introduction: `Task 3. You are going to give an interview. You have to answer five questions. Give full answers to the questions (2-3 sentences).
+    Remember that you have 40 seconds to answer each question`,
+    introduction1: "You are going to give an interview. You have to answer five questions. Give full answers to the questions (2-3 sentences).",    
+    introduction2: "Remember that you have 40 seconds to answer each question.",
+    interviewer: ["What are some common career paths for teenagers in Russia? What are you going to be?",
+     "What challenges do Russian teenagers face when trying to find a job?", 
+     "What role do parents play in helping their children choose a future job in Russia? Do your parents help you to choose your future career?",
+      "How has the job market for teenagers in Russia changed over the years?",
+      "In your opinion, what is the most important factor for Russian teenagers to consider when choosing a future job?"]
+  },
+  task4:
+  {
+    header: `Task 4. Imagine that you and your friend are doing a school project “Jobs”. You have found some photos to illustrate it but for technical reasons you cannot send them now.  Leave a voice message to your friend explaining your choice of photos and sharing some ideas about the project.  In 2.5 minutes be ready to:`,
+    text: `
+    ●	explain the choice of the illustrations for the project by briefly describing them and noting the differences;<br>
+    ●	mention the advantages (1–2) of the two jobs;<br>
+    ●	mention the disadvantages (1–2) of the two jobs;<br>
+    ●	express your opinion on the subject of the project – which of the jobs presented in the pictures you’d prefer and why.`,
+    images: ['tasks/5/4-1.jpg', 'tasks/5/4-2.jpg'],
+    text2: "<strong>You will speak for not more than 3 minutes (12-15 sentences). You have to talk continuously.</strong>"
+  },
+  filename: 'tasks/5/task5.pdf'
+}
+
+
+const AllTasks = [0, Task1, Task2, Task3, Task4, Task5]
 
 let Sounds = {
   sound1: 'sounds/sound01.mp3',
